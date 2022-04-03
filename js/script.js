e2907bdb475bdf1b6195e1dc298c9033348f0dad
Kritesh <72870567+Kritesh2nd@users.noreply.github.com>
2022-04-03 16:45:07 +0545

    1

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/*
+var inrgmblock = document.querySelector(".inrgmblock");
+var inrgmblock = document.querySelectorAll(".inrgmblock");
+function boxgenerator(){
+  var lenh=100,lenw=100,lent=0,lenl=100;
+  boxx1[widthx1] = document.createElement('div');
+  boxx1[widthx1].innerHTML = "CAT";
+  boxx1[widthx1].style.height = lenh + "px";
+  boxx1[widthx1].style.width = lenw + "px";
+  boxx1[widthx1].style.border = "1px solid #000";
+  boxx1[widthx1].style.position = "absolute";
+  boxx1[widthx1].display = "flex";
+  boxx1[widthx1].justifyContent = "center";
+  boxx1[widthx1].alignItems = "center";
+  boxx1[widthx1].style.top = lent + "px";
+  boxx1[widthx1].style.left = lenl + "px";
+  boxgenerator.appendChild(boxx1[widthx1]);
+}
+function boxgenerator(){
+  boxx1[widthx1] = document.createElement('div');
+  boxx1[widthx1].innerHTML = "CAT";
+  boxx1[widthx1].style.height = lenh + "px";
+  boxx1[widthx1].style.width = lenw + "px";
+  boxx1[widthx1].style.border = "1px solid #000";
+  boxx1[widthx1].style.position = "absolute";
+  boxx1[widthx1].display = "flex";
+  boxx1[widthx1].justifyContent = "center";
+  boxx1[widthx1].alignItems = "center";
+  boxx1[widthx1].style.top = lent + "px";
+  boxx1[widthx1].style.left = lenl + "px";
+  boxx1[widthx1].setAttribute('class', 'boxbtn');
+  boxgenerator.appendChild(boxx1[widthx1]);
+}
+document.addEventListener('keydown',function(event){
+  W=87,A=65,S=83,D=68,K=75,L=76,SPACE=32,ENTER=13
+  if(event.keyCode == 87){//W
+  }
+  else if(event.keyCode == 65){//A
+  }
+});
+Math.random() * (max - min) + min;
+b = Math.floor(Math.random()*(8-2)+2);
+src="js/script.js"
+*/
